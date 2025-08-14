e7175be7413ae5963ed7194982e747bda377657c
nononononno <131845675+nononononno@users.noreply.github.com>
2025-08-15 01:03:12 +0800

    Update test_GPSDiffusion_sdxl.py

--- test_GPSDiffusion_sdxl.py
+++ test_GPSDiffusion_sdxl.py
@@ -196,8 +196,7 @@ class TestDataset(Dataset):
             "unet_added_conditions": {
                 "text_embeds": self.text_embeds[idx],
                 "time_ids": self.time_ids[idx]
-            },
-            "name": self.all_name[idx]
+            }
         }
 
 class IPAdapter(torch.nn.Module):
